07d8ed351cd1d1d73d55fd4310afff988650c835
unknown <Steve Jang>
2018-04-01 15:49:11 +0900

    login and signup

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+# include 왜 넣는 거지?
 from . import views
+from . views import signup
+from . views import login
+
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login')
 ]
