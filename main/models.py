c636fb0f9f51b48e37ba16fa8f04e11f6543ba79
Opti <Opti@Opti_N>
2018-04-01 18:44:13 +0900

    Commit and Push Test

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from unittest.util import _MAX_LENGTH
 
 # Create your models here.
+
+class ChatRoom(models.Model):
+    key_no = models.CharField(max_length=100)
+    name = models.CharField(max_length=300)
+    create_time = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.name
