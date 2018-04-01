9f5afa3846f9566d24e9dc4e8dd9a3aec8d7150b
Opti <Opti@Opti_N>
2018-04-01 22:46:59 +0900

    url 패턴 및 view 작성 중

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    url(r'^chatroom/$', views.room, name='room'),
+    url(r'^chatroom/(?P<room_id>\d+)`/$', views.detail, name='detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from main.models import ChatRoom
 # Create your views here.
 
 def index(request):
     return render(request, "index.html", {})
+
+# 채팅방 리스트
+def room(request):
+    room_list = ChatRoom.object.all().order_by(create_time)
+    context = {'room_list': room_list}
+    return render(request, 'chatroom/room.html', context)
+
+# 채팅방 입장
+def detail(request):
+    return render(request, 'chatroom/detail.html', context)
