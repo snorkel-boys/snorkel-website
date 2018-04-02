dd895b1281f40129ec5de734af2734a1fa68e2c2
Opti <Opti@Opti_N>
2018-04-02 22:34:59 +0900

    .

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 # 헌수 궁금증) include 왜 넣는 거지? 
+from django.conf.urls import url
 from . import views
 from . views import signup
 from . views import login
@@ -8,6 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
-    path(r'^chatroom/$', views.room, name='room'),
-    path(r'^chatroom/(?P<room_id>\d+)`/$', views.detail, name='detail'),
+    url(r'^chatroom/$', views.room, name='room'),
+    url(r'^chatroom/(?P<room_id>\d+)`/$', views.detail, name='detail'),
 ]
